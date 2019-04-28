2f1743f25400a4698172191caaa2dc55b7efd954
Adam Begeman <adam.begeman@du.edu>
2019-04-28 10:55:34 -0600

    Added new shit

--- OrganoBattle2.0/Chemical2.gd
+++ OrganoBattle2.0/Chemical2.gd
@@ -3,10 +3,10 @@ extends TextureRect
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var isFinal
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	isFinal = false
+	get_tree().get_root().get_node("BattleWindow").isFinal = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
