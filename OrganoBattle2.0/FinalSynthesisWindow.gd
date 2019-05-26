fe54e1a327c841710b74473ddc1a2570e8ca9887
Tadeusz Wroblewski <teddy.wrob@gmail.com>
2019-05-25 22:05:19 -0600

    GraphicsVamp

--- OrganoBattle2.0/FinalSynthesisWindow.gd
+++ OrganoBattle2.0/FinalSynthesisWindow.gd
@@ -8,16 +8,19 @@ extends TextureRect
 func _ready():
 	global.isFinalChemical = true
 	if (global.currentChemical == global.targetChemical) :
-		 print("sucess")
-		 global.enemyHealth -= 10
+		get_node("SuccessPage").show()
+		print("sucess")
+		global.enemyHealth -= 10
 	else :
 		print("failed")
+		get_node("FailurePage").show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-func _on_TextureButton_pressed():
+func _on_ContinueButton_pressed():
 	global.isComputerTurn = true
 	get_tree().change_scene("res://FightingWindow.tscn")
+	
