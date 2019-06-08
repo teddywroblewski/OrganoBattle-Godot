fd161d63d4e843997c3eabe6ba546a893cb8a22d
Tadeusz Wroblewski <teddy.wrob@gmail.com>
2019-06-08 13:16:17 -0600

    Fixes

--- OrganoBattle2.0/Chemical2.gd
+++ OrganoBattle2.0/Chemical2.gd
@@ -13,10 +13,13 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
+#Button Pressed Functions
 func _on_Option_1_pressed():
+	get_node("DehydrationPopup").hide()
 	get_node("Dehydration").hide()
 	get_node("PBr3").hide()
+	get_node("HHalide").hide()
+	get_node("SOCl2").hide()
 	nextChemical = parent.get_node("2M2B")
 	global.power -= 1 
 	var t = Timer.new()
@@ -33,8 +36,11 @@ func _on_Option_1_pressed():
 	self.hide()
 	nextChemical.show()
 func _on_PBr3_pressed():
+	get_node("PBr3Popup").hide()
 	get_node("Dehydration").hide()
 	get_node("PBr3").hide()
+	get_node("HHalide").hide()
+	get_node("SOCl2").hide()
 	nextChemical = parent.get_node("2Br3MB")
 	global.power -= 1 
 	var t = Timer.new()
@@ -50,5 +56,64 @@ func _on_PBr3_pressed():
 	global.currentChemical = "2Br3MB"
 	self.hide()
 	nextChemical.show()
+func _on_HHalide_pressed():
+	get_node("HHalidePopup").hide()
+	get_node("Dehydration").hide()
+	get_node("PBr3").hide()
+	get_node("HHalide").hide()
+	get_node("SOCl2").hide()
+	nextChemical = parent.get_node("2I3MB")
+	global.power -= 1 
+	var t = Timer.new()
+	t.set_wait_time(8)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	get_node("3M2Boh-2I3MB-HHalide").show()
+	get_node("3M2Boh").hide()
+	get_node("3M2Boh-2I3MB-HHalide").play()
+	yield(t, "timeout")
+	t.queue_free()
+	global.currentChemical = "2I3MB"
+	self.hide()
+	nextChemical.show()
+func _on_SOCl2_pressed():
+	get_node("SOCl2Popup").hide()
+	get_node("Dehydration").hide()
+	get_node("PBr3").hide()
+	get_node("HHalide").hide()
+	get_node("SOCl2").hide()
+	nextChemical = parent.get_node("2Cl3MB")
+	global.power -= 1 
+	var t = Timer.new()
+	t.set_wait_time(8)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	get_node("3M2Boh-2Cl3MB-SOCl2").show()
+	get_node("3M2Boh").hide()
+	get_node("3M2Boh-2Cl3MB-SOCl2").play()
+	yield(t, "timeout")
+	t.queue_free()
+	global.currentChemical = "2Cl3MB"
+	self.hide()
+	nextChemical.show()
 	
-#get_tree().change_scene("res://FinalSynthesisWindow.tscn")
+#Hover Functions
+func _on_DehydrationPopup_mouse_entered():
+	get_node("DehydrationPopup").show()
+func _on_DehydrationPopup_mouse_exited():
+	get_node("DehydrationPopup").hide()
+func _on_PBr3Popup_mouse_entered():
+	get_node("PBr3Popup").show()
+func _on_PBr3Popup_mouse_exited():
+	get_node("PBr3Popup").hide()
+func _on_HHalidePopup_mouse_entered():
+	get_node("HHalidePopup").show()
+func _on_HHalidePopup_mouse_exited():
+	get_node("HHalidePopup").hide()
+func _on_SOCl2Popup_mouse_entered():
+	get_node("SOCl2Popup").show()
+func _on_SOCl2Popup_mouse_exited():
+	get_node("SOCl2Popup").hide()
+
